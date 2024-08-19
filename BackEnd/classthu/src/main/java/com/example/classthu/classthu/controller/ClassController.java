01bb05cfbdf14154a6089d98dc1eb6bf025686a2
Thao <hosithao1622004@gmail.com>
2024-08-19 23:45:05 +0700

    Push data up to the class of student page

--- BackEnd/classthu/src/main/java/com/example/classthu/classthu/controller/ClassController.java
+++ BackEnd/classthu/src/main/java/com/example/classthu/classthu/controller/ClassController.java
@@ -4,8 +4,11 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,20 +16,25 @@ import com.example.classthu.classthu.model.Classes;
 import com.example.classthu.classthu.service.ClassService;
 
 @RestController
-@RequestMapping("/student")
-public class StudentController {
+@RequestMapping("/class")
+public class ClassController {
 
     @Autowired
     private ClassService classService;
 
     @GetMapping("allClasses")
-    public List<Classes> getAllClasses() {
+    public ResponseEntity<List<Classes>> getAllClasses() {
         return classService.getAllClasses();
     }
 
     @GetMapping("classById/{id}")
-    public Optional<Classes> getClassById(@PathVariable Integer id) {
+    public ResponseEntity<Optional<Classes>> getClassById(@PathVariable Integer id) {
         return classService.getClassById(id);
+    }
+
+    @PostMapping("add")
+    public ResponseEntity<String> addClass(@RequestBody Classes dataClasses) {
+        return classService.addClass(dataClasses);
     }
 
 }
