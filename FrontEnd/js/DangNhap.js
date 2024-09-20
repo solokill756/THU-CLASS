7d446f944f59fe67f55832be73c0f71cf3f7bf1e
Thao <hosithao1622004@gmail.com>
2024-09-20 23:37:42 +0700

    fetch api for login and sign up

--- FrontEnd/js/DangNhap.js
+++ FrontEnd/js/DangNhap.js
@@ -1,9 +1,11 @@
 const signInEmail = document.querySelector('#signInEmail');
 const signInPassword = document.querySelector('#signInPassword');
-const signInPasswordAgain = document.querySelector('#SignInPasswordAgain'); // Đảm bảo tên khớp với ID trong form
+const signInPasswordAgain = document.querySelector('#SignInPasswordAgain'); 
+const loginPassword = document.querySelector('#loginPassword');
+const loginEmail = document.querySelector('#loginEmail');
+const loginForm = document.querySelector('#loginForm');
 
-console.log(signInPassword);
-console.log(signInPasswordAgain);
+
 
 document.querySelector('.loginPage').addEventListener('click', function() {
     document.querySelector('.login').classList.remove('d-none');
@@ -15,17 +17,25 @@ document.querySelector('.signUpPage').addEventListener('click', function() {
     document.querySelector('.login').classList.add('d-none');
 });
 
-signInEmail.addEventListener("change", function(event) {
+signInEmail.addEventListener("focus", function(event) {
     console.log(signInEmail.nextElementSibling);
     signInEmail.nextElementSibling.style.display = 'none';
 });
 
-signInPassword.addEventListener("change", function(event) {
+signInPassword.addEventListener("focus", function(event) {
     signInPassword.nextElementSibling.style.display = 'none';
 });
 
-signInPasswordAgain.addEventListener("change", function(event) {
+signInPasswordAgain.addEventListener("focus", function(event) {
     signInPasswordAgain.nextElementSibling.style.display = 'none';
+});
+
+loginEmail.addEventListener("focus", function(event) {
+    loginPassword.nextElementSibling.style.display = 'none';
+})
+
+loginPassword.addEventListener("focus", function(event) {
+    loginPassword.nextElementSibling.style.display = 'none';
 });
 
 // Gửi yêu cầu Đăng ký tới server
@@ -59,8 +69,48 @@ document.getElementById('signUpForm').addEventListener('submit', async function(
             body: JSON.stringify(data),
         });
 
-        console.log(response.status);
+        if(response.ok) {
+            const token = response.text();
+            localStorage.setItem('token', token);
+            window.location.href = '../html/MainPage.html';
+        }
     } catch (error) {
         console.log('Error:', error);
-    }
+    }   
+});
+
+
+// Gửi yêu cầu đăng nhập tới Server 
+
+loginForm.addEventListener('submit', async function(event) {
+    event.preventDefault(); // Chặn form submit ngay lập tức
+
+    const formData = new FormData(this);
+
+    const data = {};
+    formData.forEach((value, key) => {
+        data[key] = value;
+    });
+    console.log(data);
+    // Gửi request lên server (comment để debug)
+    try {
+        const response = await fetch('http://localhost:8080/Login', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(data),
+        });
+
+        if(response.ok) {
+            const token = response.text();
+            localStorage.setItem('token', token);
+            console.log('token', token);
+            // window.location.href = '../html/MainPage.html';
+        }
+        else {
+            console.error('Invalid response from server');
+            loginPassword.nextElementSibling.style.display  = 'block';
+        }
+    } catch (error) {
+        console.log('Error:', error);
+    }   
 });
